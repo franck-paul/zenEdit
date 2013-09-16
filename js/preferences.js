c755344b41f3751a01a36e7d6cd8448f5946f69d
franck <carnet.franck.paul@gmail.com>
2013-09-16 15:17:35 +0200

    Oops, missing some new files

---
+++ js/preferences.js
@@ -0,0 +1,9 @@
+var updateSampleBackground = function() {
+	// Change background image of #zenedit_sample
+	var background = $('#zenedit_background').val();
+	$('#zenedit_sample').css('background-image','url(index.php?pf=zenEdit/img/background/'+background+')');
+}
+
+$(function() {
+	$('#zenedit_background').change(updateSampleBackground).keypress(updateSampleBackground);
+});

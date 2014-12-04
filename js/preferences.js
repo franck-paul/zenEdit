c0781b63d0653167f1b61250b9060b0c560259ae
franck <carnet.franck.paul@gmail.com>
2014-12-04 17:18:54 +0100

    Typo

--- js/preferences.js
+++ js/preferences.js
@@ -2,7 +2,7 @@ var updateSampleBackground = function() {
 	// Change background image of #zenedit_sample
 	var background = $('#zenedit_background').val();
 	$('#zenedit_sample').css('background-image','url(index.php?pf=zenEdit/img/background/'+background+')');
-}
+};
 
 $(function() {
 	$('#zenedit_background').change(updateSampleBackground).keyup(updateSampleBackground);

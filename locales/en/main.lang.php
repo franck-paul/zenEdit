91c3830bd6fe20a7672d59ae246b456d1ee11ddc
franck <carnet.franck.paul@gmail.com>
2023-04-11 16:19:45 +0200

    l10n en

---
+++ locales/en/main.lang.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Zen mode for dcLegacyEditor'] = '';
+L10n::$locales['zenEdit'] = '';
+L10n::$locales['Switch to zen mode'] = '';
+L10n::$locales['Exit from zen mode'] = '';
+L10n::$locales['None'] = '';
+L10n::$locales['Light backgrounds'] = '';
+L10n::$locales['Dark backgrounds'] = '';
+L10n::$locales['Try to activate full screen in editor\'s zen mode'] = '';
+L10n::$locales['Your browser may not support this feature or it may be deactivated by the system.'] = '';
+L10n::$locales['Background:'] = '';
+L10n::$locales['Small margins (useful on small screens)'] = '';

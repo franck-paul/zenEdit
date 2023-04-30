39ef20571ae5cfd7248d79adf80675509c2e40e9
franck <carnet.franck.paul@gmail.com>
2023-04-30 17:06:50 +0200

    Release 3.1

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'zenEdit',
     'Zen mode for dcLegacyEditor',
     'Franck Paul',
-    '3.0',
+    '3.1',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([

307bdecae71f050fc72d3f61f1384d7bbfcc2899
franck <contact@open-time.net>
2026-08-13 16:48:18 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,24 +11,28 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'zenEdit',
-    'Zen mode for dcLegacyEditor',
-    'Franck Paul',
-    '8.0',
-    [
-        'date'        => '2026-08-03T10:17:44+0200',
-        'requires'    => [['core', '2.39']],
-        'permissions' => 'My',
-        'type'        => 'plugin',
-        'priority'    => 1010, // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
-        'settings'    => [
-            'pref' => '#user-options.zenEdit_prefs',
-        ],
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=zenEdit',
-        'support'    => 'https://github.com/franck-paul/zenEdit',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/zenEdit/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'zenEdit',
+        'Zen mode for dcLegacyEditor',
+        'Franck Paul',
+        '8.0',
+        [
+            'date'        => '2026-08-03T10:17:44+0200',
+            'requires'    => [['core', '2.39']],
+            'permissions' => 'My',
+            'type'        => 'plugin',
+            'priority'    => 1010, // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
+            'settings'    => [
+                'pref' => '#user-options.zenEdit_prefs',
+            ],
+
+            'details'    => 'https://open-time.net/?q=zenEdit',
+            'support'    => 'https://github.com/franck-paul/zenEdit',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/zenEdit/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
